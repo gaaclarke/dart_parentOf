469f2726fa24ba9d771b8beef2c5fe685456f5bc
Aaron Clarke <aaclarke@google.com>
2022-08-26 14:29:32 -0700

    initial

---
+++ lib/parent.dart
@@ -0,0 +1,53 @@
+const int _pathSeparator = 47;
+const int _dot = 46;
+const String _pathSeparatorString = '/';
+
+class FileSystemEntity {
+  static String parentOf(String path) {
+    int chop = 0;
+    int state = 0;
+    for (int ch in path.codeUnits.reversed) {
+      chop += 1;
+      if (state == 0) {
+        if (ch == _dot) {
+          state = 2;
+        } else if (ch == _pathSeparator) {
+          // pass
+        } else {
+          state = 1;
+        }
+      } else if (state == 1) {
+        if (ch == _pathSeparator) {
+          state = 4;
+        }
+      } else if (state == 2) {
+        if (ch == _dot) {
+          state = 3;
+        } else if (ch == _pathSeparator) {
+          return '${path.substring(0, path.length - chop)}$_pathSeparatorString..';
+        } else {
+          state = 1;
+        }
+      } else if (state == 3) {
+        if (ch == _pathSeparator) {
+          return '$path$_pathSeparatorString..';
+        } else {
+          state = 1;
+        }
+      } else if (state == 4) {
+        if (ch != _pathSeparator) {
+          return path.substring(0, path.length - chop + 1);
+        }
+      }
+    }
+    if (state == 0 || state == 4) {
+      return _pathSeparatorString;
+    } else if (state == 2) {
+      return '..';
+    } else if (state == 3) {
+      return '$path$_pathSeparatorString..';
+    } else {
+      return '..';
+    }
+  }
+}
---
+++ test/parent_test.dart
@@ -0,0 +1,82 @@
+import 'package:parent/parent.dart';
+import 'package:test/test.dart';
+import 'package:test/test.dart' as testLib;
+
+class Expect {
+  static void equals(String x, String y) {
+    expect(x, testLib.equals(y));
+  }
+}
+
+void main() {
+  test('posix', () {
+    // Expect.equals('/dir', FileSystemEntity.parentOf('/dir/file'));
+    // Expect.equals('/dir', FileSystemEntity.parentOf('/dir/file/'));
+    // Expect.equals('/dir', FileSystemEntity.parentOf('/dir//file//'));
+    // Expect.equals('/', FileSystemEntity.parentOf('/dir'));
+    // Expect.equals('/', FileSystemEntity.parentOf('/dir///'));
+    // Expect.equals('/', FileSystemEntity.parentOf('/'));
+
+    // Expect.equals('.', FileSystemEntity.parentOf('file'));
+    // Expect.equals('.', FileSystemEntity.parentOf('file//'));
+    // Expect.equals('.', FileSystemEntity.parentOf(''));
+    // Expect.equals('.', FileSystemEntity.parentOf('..'));
+    // Expect.equals('.', FileSystemEntity.parentOf('.'));
+    // Expect.equals('.', FileSystemEntity.parentOf(''));
+    // Expect.equals('/', FileSystemEntity.parentOf('/'));
+    // Expect.equals('/', FileSystemEntity.parentOf('//'));
+    // Expect.equals('/', FileSystemEntity.parentOf('//file///'));
+    // Expect.equals('dir', FileSystemEntity.parentOf('dir/file'));
+    // Expect.equals('dir', FileSystemEntity.parentOf('dir/file/'));
+    // Expect.equals('dir', FileSystemEntity.parentOf('dir/file//'));
+    // Expect.equals('dir/subdir', FileSystemEntity.parentOf('dir/subdir/file'));
+    // Expect.equals(
+    //     'dir//subdir', FileSystemEntity.parentOf('dir//subdir//file/'));
+    // Expect.equals(
+    //     'dir/sub.dir', FileSystemEntity.parentOf('dir/sub.dir/fi le///'));
+    // Expect.equals('dir/..', FileSystemEntity.parentOf('dir/../file/'));
+    // Expect.equals('dir/..', FileSystemEntity.parentOf('dir/../..'));
+    // Expect.equals('.', FileSystemEntity.parentOf('./..'));
+    // Expect.equals('..', FileSystemEntity.parentOf('../.'));
+  });
+
+  test('windows', () {
+    // Expect.equals(r'C:/dir', FileSystemEntity.parentOf(r'C:/dir/file'));
+    // Expect.equals(r'C:/dir', FileSystemEntity.parentOf(r'C:/dir/file/'));
+    // Expect.equals(r'C:\dir', FileSystemEntity.parentOf(r'C:\dir\file'));
+    // Expect.equals(r'C:\dir', FileSystemEntity.parentOf(r'C:\dir\file\'));
+    // Expect.equals(r'C:\dir', FileSystemEntity.parentOf(r'C:\dir\\file\\'));
+    // Expect.equals(r'C:\', FileSystemEntity.parentOf(r'C:\dir'));
+    // Expect.equals(r'C:\', FileSystemEntity.parentOf(r'C:\dir\/\'));
+    // Expect.equals(r'C:\', FileSystemEntity.parentOf(r'C:\'));
+    // // FileSystemEntity.isAbsolute returns false for 'C:'.
+    // Expect.equals(r'.', FileSystemEntity.parentOf(r'C:'));
+
+    // Expect.equals(r'\\server\share\dir',
+    //     FileSystemEntity.parentOf(r'\\server\share\dir\file'));
+    // Expect.equals(r'\\server\share\dir',
+    //     FileSystemEntity.parentOf(r'\\server\share\dir\file\'));
+    // Expect.equals(
+    //     r'\\server\share', FileSystemEntity.parentOf(r'\\server\share\file'));
+    // Expect.equals(r'\\server\', FileSystemEntity.parentOf(r'\\server\share'));
+    // Expect.equals(r'\\server\', FileSystemEntity.parentOf(r'\\server\share\'));
+    // Expect.equals(r'\\server\', FileSystemEntity.parentOf(r'\\server\'));
+    // Expect.equals(r'\\server/', FileSystemEntity.parentOf(r'\\server/'));
+    // Expect.equals(r'\\serve', FileSystemEntity.parentOf(r'\\serve'));
+
+    // Expect.equals(r'.', FileSystemEntity.parentOf(r'file'));
+    // Expect.equals(r'.', FileSystemEntity.parentOf(r''));
+    // Expect.equals(r'.', FileSystemEntity.parentOf(r'..'));
+    // Expect.equals(r'.', FileSystemEntity.parentOf(r'.'));
+    // Expect.equals(r'.', FileSystemEntity.parentOf(r''));
+    // Expect.equals(r'/', FileSystemEntity.parentOf(r'/'));
+    // Expect.equals(r'\', FileSystemEntity.parentOf(r'\'));
+    // Expect.equals(r'\', FileSystemEntity.parentOf(r'\file\\/'));
+    // Expect.equals(r'dir', FileSystemEntity.parentOf(r'dir\file'));
+    // Expect.equals(r'dir', FileSystemEntity.parentOf(r'dir\file\'));
+    // Expect.equals(r'dir', FileSystemEntity.parentOf(r'dir/file/'));
+    // Expect.equals(r'dir\subdir', FileSystemEntity.parentOf(r'dir\subdir\file'));
+    // Expect.equals(
+    //     r'dir\sub.dir', FileSystemEntity.parentOf(r'dir\sub.dir\fi le'));
+  });
+}
